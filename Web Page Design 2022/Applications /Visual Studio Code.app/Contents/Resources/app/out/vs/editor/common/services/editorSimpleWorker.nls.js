e00d1fe3c9f0c20361dca2d001e2b47146b7a3be
Anthoney Trejo <atrej-s636@chusd.org>
2024-10-24 10:40:58 -0700

    "Updates"

--- Web Page Design 2022/Applications /Visual Studio Code.app/Contents/Resources/app/out/vs/editor/common/services/editorSimpleWorker.nls.js
+++
@@ -1,5 +0,0 @@
-/*!--------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
- *--------------------------------------------------------*/define("vs/editor/common/services/editorSimpleWorker.nls",{"vs/base/common/platform":["_"]});
-
-//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/b3e4e68a0bc097f0ae7907b217c1119af9e03435/core/vs/editor/common/services/editorSimpleWorker.nls.js.map
